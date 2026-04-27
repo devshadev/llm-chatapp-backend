b40d5f3ae796627044fe610e3b9775363730773a
devshadev <umair22432syed@gmail.com>
2026-04-27 17:23:13 +0300

    CORS changes

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="LLM Chat App", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://llm-chatapp-backend.vercel.app","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
